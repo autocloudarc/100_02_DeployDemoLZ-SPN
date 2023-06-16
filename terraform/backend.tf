c7825bc04e5a2b5f71c35da7c255d93bd2b99a5a
Preston K. Parsard <autocloudarc@noreply.github.com>
2023-06-16 18:37:47 -0400

    state(file): update name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
       resource_group_name = "tfm-rgp-01"
       storage_account_name = "1sta1739"
       container_name = "tf-tfstate"
-      key = "tfm.tfstate"
+      key ="ex100-2-spn.tfstate"
   }
 }
